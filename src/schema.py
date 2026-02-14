26ac9a8e9184f6a39ee623e726271b369946c08f
labubu-666 <onigiri@fastmail.jp>
2026-02-15 00:42:40 +0100

    feat: support detached services

--- src/schema.py
+++ src/schema.py
@@ -9,21 +9,37 @@ def build_container_name(working_dir: str, service: str) -> str:
 
 
 class Service(BaseModel):
+    detached: bool
     image: str
 
     def build_run_command(self, container_name: str) -> list:
-        return ["docker", "run", "--name", container_name, "--rm", self.image]
+        """Build the docker run command for this service."""
+
+        command = ["docker", "run", "--name", container_name, "--rm"]
+        if bool(self.detached):
+            # docker uses --detach (or -d) to run containers in the background
+            command.append("--detach")
+        command.append(self.image)
+        return command
 
     def build_stop_command(self, container_name: str) -> list:
-        return ["docker", "stop", "--name", container_name, self.image]
+        # docker stop takes container names/ids as positional arguments
+        return ["docker", "container", "stop", container_name]
+
+    def build_remove_command(self, container_name: str) -> list:
+        # docker stop takes container names/ids as positional arguments
+        return ["docker", "container", "rm", container_name]
 
 
 class Deployment(BaseModel):
     services: Optional[Dict[str, Service]] = None
 
 
-def load_deployment(deployment_contents: str) -> Optional[Deployment]:
+def load_deployment(deployment_contents: str, detached: bool) -> Optional[Deployment]:
     if data := yaml.safe_load(deployment_contents):
+        for name, service in data.get("services", {}).items():
+            service.update({"detached": detached})
+
         return Deployment.model_validate(data)
     else:
         return None
--- tests/test_schema.py
+++ tests/test_schema.py
@@ -6,17 +6,17 @@ from src.schema import load_deployment
 
 def test_load_empty_deployment():
     deployment = load_deployment("""
-    """)
+    """, detached=False)
 
     assert deployment is None
 
 
-def test_load_single_service_with_image():
+def test_build_run_command_for_single_attached_service_with_image():
     deployment = load_deployment("""
 services:
   service-1:
     image: image-1:latest
-    """)
+    """, detached=False)
 
     assert deployment is not None
     assert isinstance(deployment.services, dict)
@@ -26,3 +26,36 @@ services:
     assert deployment.services["service-1"].build_run_command(
         container_name=container_name
     ) == ["docker", "run", "--name", "test_service-1_1", "--rm", "image-1:latest"]
+
+
+def test_build_run_command_for_single_detached_service_with_image():
+    deployment = load_deployment("""
+services:
+  service-1:
+    image: image-1:latest
+    """, detached=True)
+
+    assert deployment is not None
+    assert isinstance(deployment.services, dict)
+    assert "service-1" in deployment.services
+    assert deployment.services["service-1"].image == "image-1:latest"
+    container_name = "test_service-1_1"
+    assert deployment.services["service-1"].build_run_command(
+        container_name=container_name
+    ) == ["docker", "run", "--name", "test_service-1_1", "--rm", "--detach", "image-1:latest"]
+
+def test_build_stop_command_for_single_detached_service_with_image():
+    deployment = load_deployment("""
+services:
+  service-1:
+    image: image-1:latest
+    """, detached=True)
+
+    assert deployment is not None
+    assert isinstance(deployment.services, dict)
+    assert "service-1" in deployment.services
+    assert deployment.services["service-1"].image == "image-1:latest"
+    container_name = "test_service-1_1"
+    assert deployment.services["service-1"].build_stop_command(
+        container_name=container_name
+    ) == ["docker", "container", "stop", "test_service-1_1"]
